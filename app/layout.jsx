926383763571b5648a1e67f2e1a5b1e7dbd896e0
ravindrauiet <ravindranathjha76@gmail.com>
2026-07-18 16:26:53 +0530

    feat: update Meta Pixel IDs for improved tracking accuracy

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,7 +15,7 @@ const cormorant = Cormorant_Garamond({
   variable: "--font-display",
 });
 
-const metaPixelIds = ["358677750473605", "27881346908132910"];
+const metaPixelIds = ["1229284689266211"];
 
 const metaPixelScript = `
 !function(f,b,e,v,n,t,s)
