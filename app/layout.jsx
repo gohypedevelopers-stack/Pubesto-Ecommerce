55902f2ac5a07bcb41f3345467a6063da65fcf5d
ravindrauiet <ravindranathjha76@gmail.com>
2026-07-20 14:52:22 +0530

    feat: add Microsoft Clarity tracking script to RootLayout

--- app/layout.jsx
+++ app/layout.jsx
@@ -77,6 +77,19 @@ export default function RootLayout({ children }) {
             __html: metaPixelScript,
           }}
         />
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "xpc40dp76w");
+            `,
+          }}
+        />
         <noscript>
           {metaPixelIds.map((pixelId) => (
             <img
